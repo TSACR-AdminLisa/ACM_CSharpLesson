27cd8de230ad673ec1a8643d5966d99bee3e617a
Liza Varela Solórzano <lvarela@outlook.com>
2018-10-16 13:52:16 -0600

    cambios en clase customer

--- ACM/ACM.BL/Customer.cs
+++ ACM/ACM.BL/Customer.cs
@@ -4,10 +4,37 @@ namespace ACM.BL
 {
     public class Customer
     {
+        #region "Variables"
+
+        private string _lastName;
+
+        #endregion
+
         #region "Properties"
 
-        public string FirstName 
+        public string FirstName { get; set; }
 
+        public string LastName
+        {
+            get
+            {
+                return _lastName;
+            }
+            set
+            {
+                _lastName = value;
+            }
+        }
+
+        public string EmailAddress { get; set; }
+        public int CustomerId { get; private set; }
+        public string FullName
+        {
+            get
+            {
+                return LastName + "," + FirstName;
+            }
+        }
         #endregion
     }
 }
