d0e39e6833372785ad3eaf313d4dafbe2ca64b42
Liza Varela Solórzano <lvarela@outlook.com>
2018-10-19 12:31:51 -0600

    nuevo proyecto AcmeCustomerManagement

---
+++ AcmeCustomerManagement/AcmeServices/ACM.BL/Customer/Customer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ACM.BL
+{
+    public class Customer
+    {
+        #region "Properties"
+
+        public int CustomerId { get; set; }
+        public string EmailAddress { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        #endregion
+    }
+}
---
+++ AcmeCustomerManagement/AcmeServices/Core.Common/EmailLibrary.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Core.Common
+{
+    public class EmailLibrary
+    {
+    }
+}
