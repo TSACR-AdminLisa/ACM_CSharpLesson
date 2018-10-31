6bfeb61accdaadeeb8c1c60a6ec924ed6ac20d0b
Liza Varela Solórzano <lvarela@outlook.com>
2018-10-31 11:07:15 -0600

    nuevos proyectos de ejemplo: 1. Mastering CSharp4: para nuevas técnicas respectivas a C#.NET 4.0 2. Unit Testing: referente al uso de los frameworks NUnitTest y Moq

---
+++ MasteringCSharp4/MasteringCSharp4/ConsoleSample/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //basic of System.Dynamic
+            dynamic x = "Hello";
+            Console.WriteLine(x.Length);
+            x = new int[] { 10, 20, 30 };
+            Console.WriteLine(x.Length);
+            Console.Read();        }
+    }
+}
